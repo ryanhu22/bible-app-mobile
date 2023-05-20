6721d2dc5b5b910e666998a433e71684767a5fa1
ryanhu22 <ryanhu22@berkeley.edu>
2023-05-19 23:37:24 -0700

    added fetching from firestore for passage...this one was a headache

--- components/Passage2.js
+++ components/Passage2.js
@@ -24,9 +24,9 @@ import auth from "@react-native-firebase/auth";
 import HTML from "react-native-render-html";
 import HTMLParser from "react-native-html-parser";
 
-const Passage = ({ html_str }) => {
-  var DomParser = require("react-native-html-parser").DOMParser;
+import firestore from "@react-native-firebase/firestore";
 
+const Passage = ({ passage }) => {
   // Layout Functionalities
   const scrollViewRef = useRef(null);
   const [textLayout, setTextLayout] = useState(null);
@@ -46,6 +46,117 @@ const Passage = ({ html_str }) => {
   const [showCommentFooter, setShowCommentFooter] = useState(false);
   const [showCommentFooterInput, setShowCommentFooterInput] = useState(false);
   const [commentInput, setCommentInput] = useState("");
+
+  const [renderTags, setRenderTags] = useState([]);
+
+  const appendToRenderTags = async (newTag) => {
+    setRenderTags((prevState) => {
+      return [...prevState, newTag];
+    });
+  };
+
+  // Handle passage loading
+  useEffect(() => {
+    setRenderTags([]);
+    var tmpArray = [];
+    console.log("MY PASSAGE");
+    console.log(passage);
+    if (!passage) {
+      return;
+    }
+    const sortedKeys = Object.keys(passage).sort();
+    var key = 0;
+    for (const sortedKey of sortedKeys) {
+      var currElement = passage[sortedKey];
+      console.log(currElement);
+      // If it's a header
+      if (currElement?.isHeading && currElement.isHeading === "True") {
+        if (tmpArray.length > 0) {
+          const renderTagOld = (
+            <RegularText key={key}>
+              {tmpArray.map((item) => {
+                key += 2;
+                return (
+                  <TextNoNewline
+                    verse={item.verseNum}
+                    reset={isReset}
+                    onPress={(e) => handleTextPress(e, item.verseNum)}
+                    onLayout={handleTextLayout}
+                    yellowHighlightIds={yellowHighlightIds}
+                    comments={comments}
+                    selectedComment={currVerseComment}
+                    key={key}
+                  >
+                    <VerseText class="verse-num" id="v01001011-1" key={key + 1}>
+                      {" " + item.verseNum}
+                    </VerseText>
+                    {item.verse}
+                  </TextNoNewline>
+                );
+              })}
+            </RegularText>
+          );
+          key += 3;
+          tmpArray = [];
+          setTimeout(() => {
+            appendToRenderTags(renderTagOld);
+          }, 1000);
+          // renderTags.push(renderTagOld);
+        }
+
+        const renderTagHeader = (
+          <HeaderText key={key + 1}>{currElement.verse}</HeaderText>
+        );
+        key += 2;
+        setTimeout(() => {
+          appendToRenderTags(renderTagHeader);
+        }, 1000);
+
+        // renderTags.push(renderTagHeader);
+      } else {
+        tmpArray.push(currElement);
+      }
+
+      if (
+        currElement?.isEndParagraph &&
+        currElement.isEndParagraph === "True"
+      ) {
+        // Construct
+        const renderTag = (
+          <RegularText key={key}>
+            {tmpArray.map((item) => {
+              key += 2;
+              return (
+                <TextNoNewline
+                  verse={item.verseNum}
+                  reset={isReset}
+                  onPress={(e) => handleTextPress(e, item.verseNum)}
+                  onLayout={handleTextLayout}
+                  yellowHighlightIds={yellowHighlightIds}
+                  comments={comments}
+                  selectedComment={currVerseComment}
+                  key={key + 1}
+                >
+                  <VerseText class="verse-num" id="v01001011-1" key={key + 2}>
+                    {" " + item.verseNum}
+                  </VerseText>
+                  {item.verse}
+                </TextNoNewline>
+              );
+            })}
+          </RegularText>
+        );
+        key += 3;
+
+        tmpArray = [];
+        setTimeout(() => {
+          appendToRenderTags(renderTag);
+        }, 1000);
+
+        // renderTags.push(renderTag);
+      }
+    }
+  }, [passage]);
 
   // Handle automatic closing of footer when no underlines
   useEffect(() => {
@@ -156,316 +267,88 @@ const Passage = ({ html_str }) => {
     setTextLayout(layout);
   };
 
-  const passage_json = {
-    0: {
-      isEndParagraph: "True",
-      verse:
-        " “Beware of practicing your righteousness before other people in order to be seen by them, for then you will have no reward from your Father who is in heaven.",
-      isHeading: "False",
-      verseNum: 1,
-    },
-    1: {
-      isEndParagraph: "False",
-      verse:
-        " “Thus, when you give to the needy, sound no trumpet before you, as the hypocrites do in the synagogues and in the streets, that they may be praised by others. Truly, I say to you, they have received their reward.",
-      isHeading: "False",
-      verseNum: 2,
-    },
-    2: {
-      isEndParagraph: "False",
-      verse:
-        " But when you give to the needy, do not let your left hand know what your right hand is doing,",
-      isHeading: "False",
-      verseNum: 3,
-    },
-    3: {
-      isEndParagraph: "False",
-      verse:
-        " so that your giving may be in secret. And your Father who sees in secret will reward you.",
-      isHeading: "False",
-      verseNum: 4,
-    },
-    4: { isHeading: "True", verse: "The Lord’s Prayer" },
-    5: {
-      isEndParagraph: "False",
-      verse:
-        " “And when you pray, you must not be like the hypocrites. For they love to stand and pray in the synagogues and at the street corners, that they may be seen by others. Truly, I say to you, they have received their reward.",
-      isHeading: "False",
-      verseNum: 5,
-    },
-    6: {
-      isEndParagraph: "True",
-      verse:
-        " But when you pray, go into your room and shut the door and pray to your Father who is in secret. And your Father who sees in secret will reward you.",
-      isHeading: "False",
-      verseNum: 6,
-    },
-    7: {
-      isEndParagraph: "False",
-      verse:
-        " “And when you pray, do not heap up empty phrases as the Gentiles do, for they think that they will be heard for their many words.",
-      isHeading: "False",
-      verseNum: 7,
-    },
-    8: {
-      isEndParagraph: "False",
-      verse:
-        " Do not be like them, for your Father knows what you need before you ask him.",
-      isHeading: "False",
-      verseNum: 8,
-    },
-    9: {
-      isEndParagraph: "False",
-      verse:
-        " Pray then like this:\n\n    “Our Father in heaven,\n    hallowed be your name.\n",
-      isHeading: "False",
-      verseNum: 9,
-    },
-    10: {
-      isEndParagraph: "False",
-      verse:
-        " Your kingdom come,\n    your will be done,\n        on earth as it is in heaven.\n",
-      isHeading: "False",
-      verseNum: 10,
-    },
-    11: {
-      isEndParagraph: "False",
-      verse: " Give us this day our daily bread,\n",
-      isHeading: "False",
-      verseNum: 11,
-    },
-    12: {
-      isEndParagraph: "False",
-      verse:
-        " and forgive us our debts,\n        as we also have forgiven our debtors.\n",
-      isHeading: "False",
-      verseNum: 12,
-    },
-    13: {
-      isEndParagraph: "False",
-      verse:
-        " And lead us not into temptation,\n        but deliver us from evil.\n    \n    \n",
-      isHeading: "False",
-      verseNum: 13,
-    },
-    14: {
-      isEndParagraph: "False",
-      verse:
-        " For if you forgive others their trespasses, your heavenly Father will also forgive you,",
-      isHeading: "False",
-      verseNum: 14,
-    },
-    15: {
-      isEndParagraph: "False",
-      verse:
-        " but if you do not forgive others their trespasses, neither will your Father forgive your trespasses.",
-      isHeading: "False",
-      verseNum: 15,
-    },
-    16: { isHeading: "True", verse: "Fasting" },
-    17: {
-      isEndParagraph: "False",
-      verse:
-        " “And when you fast, do not look gloomy like the hypocrites, for they disfigure their faces that their fasting may be seen by others. Truly, I say to you, they have received their reward.",
-      isHeading: "False",
-      verseNum: 16,
-    },
-    18: {
-      isEndParagraph: "False",
-      verse: " But when you fast, anoint your head and wash your face,",
-      isHeading: "False",
-      verseNum: 17,
-    },
-    19: {
-      isEndParagraph: "False",
-      verse:
-        " that your fasting may not be seen by others but by your Father who is in secret. And your Father who sees in secret will reward you.",
-      isHeading: "False",
-      verseNum: 18,
-    },
-    20: { isHeading: "True", verse: "Lay Up Treasures in Heaven" },
-    21: {
-      isEndParagraph: "False",
-      verse:
-        " “Do not lay up for yourselves treasures on earth, where moth and rust destroy and where thieves break in and steal,",
-      isHeading: "False",
-      verseNum: 19,
-    },
-    22: {
-      isEndParagraph: "False",
-      verse:
-        " but lay up for yourselves treasures in heaven, where neither moth nor rust destroys and where thieves do not break in and steal.",
-      isHeading: "False",
-      verseNum: 20,
-    },
-    23: {
-      isEndParagraph: "True",
-      verse: " For where your treasure is, there your heart will be also.",
-      isHeading: "False",
-      verseNum: 21,
-    },
-    24: {
-      isEndParagraph: "False",
-      verse:
-        " “The eye is the lamp of the body. So, if your eye is healthy, your whole body will be full of light,",
-      isHeading: "False",
-      verseNum: 22,
-    },
-    25: {
-      isEndParagraph: "True",
-      verse:
-        " but if your eye is bad, your whole body will be full of darkness. If then the light in you is darkness, how great is the darkness!",
-      isHeading: "False",
-      verseNum: 23,
-    },
-    26: {
-      isEndParagraph: "False",
-      verse:
-        " “No one can serve two masters, for either he will hate the one and love the other, or he will be devoted to the one and despise the other. You cannot serve God and money.",
-      isHeading: "False",
-      verseNum: 24,
-    },
-    27: { isHeading: "True", verse: "Do Not Be Anxious" },
-    28: {
-      isEndParagraph: "False",
-      verse:
-        " “Therefore I tell you, do not be anxious about your life, what you will eat or what you will drink, nor about your body, what you will put on. Is not life more than food, and the body more than clothing?",
-      isHeading: "False",
-      verseNum: 25,
-    },
-    29: {
-      isEndParagraph: "False",
-      verse:
-        " Look at the birds of the air: they neither sow nor reap nor gather into barns, and yet your heavenly Father feeds them. Are you not of more value than they?",
-      isHeading: "False",
-      verseNum: 26,
-    },
-    30: {
-      isEndParagraph: "False",
-      verse:
-        " And which of you by being anxious can add a single hour to his span of life?",
-      isHeading: "False",
-      verseNum: 27,
-    },
-    31: {
-      isEndParagraph: "False",
-      verse:
-        " And why are you anxious about clothing? Consider the lilies of the field, how they grow: they neither toil nor spin,",
-      isHeading: "False",
-      verseNum: 28,
-    },
-    32: {
-      isEndParagraph: "False",
-      verse:
-        " yet I tell you, even Solomon in all his glory was not arrayed like one of these.",
-      isHeading: "False",
-      verseNum: 29,
-    },
-    33: {
-      isEndParagraph: "False",
-      verse:
-        " But if God so clothes the grass of the field, which today is alive and tomorrow is thrown into the oven, will he not much more clothe you, O you of little faith?",
-      isHeading: "False",
-      verseNum: 30,
-    },
-    34: {
-      isEndParagraph: "False",
-      verse:
-        " Therefore do not be anxious, saying, ‘What shall we eat?’ or ‘What shall we drink?’ or ‘What shall we wear?’",
-      isHeading: "False",
-      verseNum: 31,
-    },
-    35: {
-      isEndParagraph: "False",
-      verse:
-        " For the Gentiles seek after all these things, and your heavenly Father knows that you need them all.",
-      isHeading: "False",
-      verseNum: 32,
-    },
-    36: {
-      isEndParagraph: "True",
-      verse:
-        " But seek first the kingdom of God and his righteousness, and all these things will be added to you.",
-      isHeading: "False",
-      verseNum: 33,
-    },
-    37: {
-      isEndParagraph: "False",
-      verse:
-        " “Therefore do not be anxious about tomorrow, for tomorrow will be anxious for itself. Sufficient for the day is its own trouble.",
-      isHeading: "False",
-      verseNum: 34,
-    },
-  };
+  // var renderTags = [];
+  // var tmpArray = [];
+  // for (let i = 0; i < Object.keys(passage).length; i++) {
+  //   var currElement = passage[i];
+  //   // If it's a header
+  //   if (currElement.isHeading === "True") {
+  //     if (tmpArray.length > 0) {
+  //       const renderTagOld = (
+  //         <RegularText key={Object.keys(renderTags).length}>
+  //           {tmpArray.map((item) => {
+  //             return (
+  //               <TextNoNewline
+  //                 verse={item.verseNum}
+  //                 reset={isReset}
+  //                 onPress={(e) => handleTextPress(e, item.verseNum)}
+  //                 onLayout={handleTextLayout}
+  //                 yellowHighlightIds={yellowHighlightIds}
+  //                 comments={comments}
+  //                 selectedComment={currVerseComment}
+  //                 key={item.verseNum}
+  //               >
+  //                 <VerseText
+  //                   class="verse-num"
+  //                   id="v01001011-1"
+  //                   key={item.verseNum}
+  //                 >
+  //                   {" " + item.verseNum}
+  //                 </VerseText>
+  //                 {item.verse}
+  //               </TextNoNewline>
+  //             );
+  //           })}
+  //         </RegularText>
+  //       );
+  //       tmpArray = [];
+  //       renderTags.push(renderTagOld);
+  //     }
 
-  var renderTags = [];
-  var tmpArray = [];
-  for (let i = 0; i < Object.keys(passage_json).length; i++) {
-    var currElement = passage_json[i];
-    // If it's a header
-    if (currElement.isHeading === "True") {
-      const renderTagOld = (
-        <RegularText>
-          {tmpArray.map((item) => {
-            return (
-              <TextNoNewline
-                verse={item.verseNum}
-                reset={isReset}
-                onPress={(e) => handleTextPress(e, item.verseNum)}
-                onLayout={handleTextLayout}
-                yellowHighlightIds={yellowHighlightIds}
-                comments={comments}
-                selectedComment={currVerseComment}
-              >
-                <VerseText class="verse-num" id="v01001011-1">
-                  {" " + item.verseNum}
-                </VerseText>
-                {item.verse}
-              </TextNoNewline>
-            );
-          })}
-        </RegularText>
-      );
+  //     const renderTagHeader = (
+  //       <HeaderText key={Object.keys(renderTags).length}>
+  //         {currElement.verse}
+  //       </HeaderText>
+  //     );
+  //     renderTags.push(renderTagHeader);
+  //   } else {
+  //     tmpArray.push(currElement);
+  //   }
 
-      const renderTagHeader = <HeaderText>{currElement.verse}</HeaderText>;
+  //   if (currElement.isEndParagraph && currElement.isEndParagraph === "True") {
+  //     // Construct
+  //     const renderTag = (
+  //       <RegularText key={Object.keys(renderTags).length}>
+  //         {tmpArray.map((item) => {
+  //           return (
+  //             <TextNoNewline
+  //               verse={item.verseNum}
+  //               reset={isReset}
+  //               onPress={(e) => handleTextPress(e, item.verseNum)}
+  //               onLayout={handleTextLayout}
+  //               yellowHighlightIds={yellowHighlightIds}
+  //               comments={comments}
+  //               selectedComment={currVerseComment}
+  //               key={item.verseNum}
+  //             >
+  //               <VerseText
+  //                 class="verse-num"
+  //                 id="v01001011-1"
+  //                 key={item.verseNum}
+  //               >
+  //                 {" " + item.verseNum}
+  //               </VerseText>
+  //               {item.verse}
+  //             </TextNoNewline>
+  //           );
+  //         })}
+  //       </RegularText>
+  //     );
 
-      tmpArray = [];
-      renderTags.push(renderTagOld);
-      renderTags.push(renderTagHeader);
-    } else {
-      tmpArray.push(currElement);
-    }
-
-    if (currElement.isEndParagraph && currElement.isEndParagraph === "True") {
-      // Construct
-      const renderTag = (
-        <RegularText>
-          {tmpArray.map((item) => {
-            return (
-              <TextNoNewline
-                verse={item.verseNum}
-                reset={isReset}
-                onPress={(e) => handleTextPress(e, item.verseNum)}
-                onLayout={handleTextLayout}
-                yellowHighlightIds={yellowHighlightIds}
-                comments={comments}
-                selectedComment={currVerseComment}
-              >
-                <VerseText class="verse-num" id="v01001011-1">
-                  {" " + item.verseNum}
-                </VerseText>
-                {item.verse}
-              </TextNoNewline>
-            );
-          })}
-        </RegularText>
-      );
-
-      tmpArray = [];
-      renderTags.push(renderTag);
-      console.log(renderTags);
-    }
-  }
+  //     tmpArray = [];
+  //     renderTags.push(renderTag);
+  //   }
+  // }
 
   return (
     <View style={{ flex: 1, flexDirection: "column" }}>
--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,14 +1,26 @@
 import { StyleSheet, SafeAreaView, View, Text } from "react-native";
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Header from "../components/Header";
 import Passage from "../components/Passage";
 import Passage2 from "../components/Passage2";
 
+import firestore from "@react-native-firebase/firestore";
+
 const HomeScreen = () => {
+  useEffect(() => {
+    search();
+  }, []);
+
+  const [passage, setPassage] = useState(null);
+
+  const search = async () => {
+    const coll = await firestore().collection("Genesis").doc("3").get();
+    setPassage(coll._data);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Header />
-      <Passage2 />
+      <Passage2 passage={passage} />
     </SafeAreaView>
   );
 };
